040b536fa488f895f22b09b601d37b234d57657d
Prithviraj Nimbalkar <prithvirajnimbalkar3@gmail.com>
2026-08-17 23:57:59 +0530

    Add files via upload

---
+++ variables & Data Types/variables.py
@@ -0,0 +1,38 @@
+# def -A variable is a named storage location in memory used to hold a value that can change during a program.
+
+a=123
+print(a)
+
+
+e=1.1
+print(e)
+
+prithviraj=9
+
+# b=prithviraj
+# print(b)
+
+# c= "prithvi"
+b="prithviraj"
+print(b)
+
+d= True
+#  datatypes 
+
+# types of  datatypes
+
+a1=9
+print(a+a1)
+
+f=complex(8,2)
+print(f)
+
+print('the type of a is',type(a))
+print('the type of b is',type(b))
+# print('the type of c is',type(c))
+print('the type of d is',type(d))
+print('the type of e is',type(e))
+print('the type of f is',type(f))
+
+# build in datatypes
+
